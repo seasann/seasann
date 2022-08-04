f298f7a814cd8883a822fe2d085dcae1844d7d8b
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2022-08-04 19:27:12 +0300

    Added prettier and husky

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
+import { exec } from 'child_process';
 import inquirer from 'inquirer';
-import { exec } from "child_process";
-const version = "Seasann v0.4.1";
+const version = 'Seasann v0.4.1';
 
-
-function createNewProj(name){
-  exec(`git clone https://github.com/micziz/seasann-template.git ${name}`, (error) => {
-    if (error) {
+function createNewProj(name) {
+  exec(
+    `git clone https://github.com/micziz/seasann-template.git ${name}`,
+    (error) => {
+      if (error) {
         console.log(`error: ${error.message}`);
         return;
+      }
     }
-  });
+  );
   console.log(`Done! Project created at ${name}`);
-  console.log("Next steps:")
-  console.log(`\n     cd ${name}`)
-  console.log("     npm i")
-  console.log("To run the builder")
-  console.log("\n     node .")
-  console.log("     node express.cjs\n")
-
+  console.log('Next steps:');
+  console.log(`\n     cd ${name}`);
+  console.log('     npm i');
+  console.log('To run the builder');
+  console.log('\n     node .');
+  console.log('     node express.cjs\n');
 }
 
-function initProj(name){
+function initProj(name) {
   exec(`git init`, (error) => {
     if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+      console.log(`error: ${error.message}`);
+      return;
     }
   });
-  exec(`git pull https://github.com/micziz/seasann-template.git --allow-unrelated-histories`, (error) => {
-    if (error) {
+  exec(
+    `git pull https://github.com/micziz/seasann-template.git --allow-unrelated-histories`,
+    (error) => {
+      if (error) {
         console.log(`error: ${error.message}`);
         return;
+      }
     }
-  });
+  );
   console.log(`Done! Project initialized at ${name}`);
-  console.log("Next steps:")
-  console.log("     npm i")
-  console.log("To run the builder")
-  console.log("\n     node .")
-  console.log("     node express.cjs\n")
+  console.log('Next steps:');
+  console.log('     npm i');
+  console.log('To run the builder');
+  console.log('\n     node .');
+  console.log('     node express.cjs\n');
 }
 
-
 let args = process.argv;
-if (args[2] == "new"){
+if (args[2] == 'new') {
   async function askNameOfProject() {
     const answers = await inquirer.prompt({
       name: 'project_name',
@@ -53,12 +56,12 @@ if (args[2] == "new"){
         return 'seasann-app';
       },
     });
-  
-    return createNewProj(answers.project_name)
+
+    return createNewProj(answers.project_name);
   }
 
-  await askNameOfProject()
-} else if (args[2] == "init"){
+  await askNameOfProject();
+} else if (args[2] == 'init') {
   async function askNameOfProject() {
     const answers = await inquirer.prompt({
       name: 'project_name',
@@ -68,10 +71,10 @@ if (args[2] == "new"){
         return 'seasann-app';
       },
     });
-  
-    return initProj(answers.project_name)
+
+    return initProj(answers.project_name);
   }
-  await askNameOfProject()
-}else if (args[2] == "--version"){
-  console.log(version)
+  await askNameOfProject();
+} else if (args[2] == '--version') {
+  console.log(version);
 }
