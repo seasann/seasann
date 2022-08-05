ceb2de86fe431a852ddbbf82fbf43f8578c8fcf1
micziz <miczicontent@gmail.com>
2022-08-05 13:53:43 +0200

    Test new build command

--- build/projectCreation/createNewProject.js
+++ build/projectCreation/createNewProject.js
@@ -1,25 +1,22 @@
 import { exec } from 'child_process';
 let args = process.argv;
 export function createNewProj(name) {
-  exec(
-    `git clone https://github.com/micziz/seasann-template.git ${name}`,
-    (error) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
+    exec(`git clone https://github.com/micziz/seasann-template.git ${name}`, (error) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+    });
+    console.log(`Done! Project created at ${name}`);
+    if (args[3] != '--silent') {
+        console.log('Next steps:');
+        console.log(`\n     cd ${name}`);
+        console.log('     npm i');
     }
-  );
-  console.log(`Done! Project created at ${name}`);
-  if (args[3] != '--silent') {
-    console.log('Next steps:');
-    console.log(`\n     cd ${name}`);
-    console.log('     npm i');
-  }
-  if (args[3] == '--verbose') {
-    console.log('\nTo build the app');
-    console.log('\n       npm run build');
-    console.log('\nTo start a server');
-    console.log('\n       npm run dev\n');
-  }
+    if (args[3] == '--verbose') {
+        console.log('\nTo build the app');
+        console.log('\n       npm run build');
+        console.log('\nTo start a server');
+        console.log('\n       npm run dev\n');
+    }
 }
