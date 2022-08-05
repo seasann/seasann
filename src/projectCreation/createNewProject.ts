38276fb343592a3f4f9253d901c385b937565c2a
micziz <miczicontent@gmail.com>
2022-08-05 15:07:05 +0200

    Build

--- src/projectCreation/createNewProject.ts
+++ src/projectCreation/createNewProject.ts
@@ -16,39 +16,36 @@ export function createNewProj(name: any) {
   if (args[3] != '--silent') {
     console.log('Next steps:');
     console.log(`\n     cd ${name}`);
-    detect()
-      .then(pm => {
-        if (pm == "npm"){
-          console.log('     npm install');      
-        } else if (pm == "yarn"){
-          console.log('     yarn install')
-        } else if (pm == "pnpm"){
-          console.log('     pnpm install')
-        }
-    })
+    detect().then((pm) => {
+      if (pm == 'npm') {
+        console.log('     npm install');
+      } else if (pm == 'yarn') {
+        console.log('     yarn install');
+      } else if (pm == 'pnpm') {
+        console.log('     pnpm install');
+      }
+    });
   }
   if (args[3] == '--verbose') {
     console.log('\nTo build the app');
-    detect()
-      .then(pm => {
-        if (pm == "npm"){
-          console.log('\n       npm run build');
-        } else if (pm == "yarn"){
-          console.log('\n       yarn build');
-        } else if (pm == "pnpm"){
-          console.log('\n       pnpm build');
-        }
-    })
+    detect().then((pm) => {
+      if (pm == 'npm') {
+        console.log('\n       npm run build');
+      } else if (pm == 'yarn') {
+        console.log('\n       yarn build');
+      } else if (pm == 'pnpm') {
+        console.log('\n       pnpm build');
+      }
+    });
     console.log('\nTo start a server');
-    detect()
-      .then(pm => {
-        if (pm == "npm"){
-          console.log('\n       npm run dev');
-        } else if (pm == "yarn"){
-          console.log('\n       yarn dev');
-        } else if (pm == "pnpm"){
-          console.log('\n       pnpm dev');
-        }
-    })
+    detect().then((pm) => {
+      if (pm == 'npm') {
+        console.log('\n       npm run dev');
+      } else if (pm == 'yarn') {
+        console.log('\n       yarn dev');
+      } else if (pm == 'pnpm') {
+        console.log('\n       pnpm dev');
+      }
+    });
   }
 }
