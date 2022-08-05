be76c0dc060811848cbc5678608e6468dd9cde18
micziz <miczicontent@gmail.com>
2022-08-05 17:48:24 +0200

    0.5.1

--- src/functions/help.ts
+++ src/functions/help.ts
@@ -2,7 +2,7 @@ export function help() {
   console.log('Usage: node . command [options]');
   console.log('   --help, -h: Print this messeage and exit');
   console.log('   --version, -V: Print version and exit');
-  console.log('   --ts: add typescript.')
+  console.log('   --ts: add typescript.');
   console.log('   new: Create a new project');
   console.log(
     '   init: Initialize new project in the current working directory'
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import inquirer from 'inquirer';
 import { initProj } from './projectCreation/initProj.js';
 import { createNewProj } from './projectCreation/createNewProject.js';
 import { help } from './functions/help.js';
-const version = 'Seasann v0.5.0';
+const version = 'Seasann v0.5.1';
 const args = process.argv;
 
 if (args[2] == 'new') {
